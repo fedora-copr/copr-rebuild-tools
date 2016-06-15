33f512386640fd512be7b78dbb9678ddcc086a64
Jakub Kadlčík <frostyx@email.cz>
2016-06-15 18:54:04 +0200

    Implement rubygems backend

--- backends/rubygems.py
+++ backends/rubygems.py
@@ -1,9 +1,18 @@
+from subprocess import PIPE, Popen, call
 from copr_rebuild_tools import Backend, CoprBackend
 
 
 class Rubygems(Backend):
-    pass
+    def get_all(self):
+        # Require `rubygems` package
+        cmd = ["gem", "search"]
+        proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        output, error = proc.communicate()
+        return [x.split()[0] for x in output.split("\n")[:-1]]
 
 
 class CoprRubygems(CoprBackend):
-    pass
+    def submit(self, package):
+        command = ["/usr/bin/copr-cli", "--config", self.conf["copr-config"],
+                   "buildgem", self.copr_full_name, "--gem", package, "--nowait"]
+        call(command)
