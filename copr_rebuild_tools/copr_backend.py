160e01b38be49b677e7be028e448bd2e900c8d16
Jakub Kadlčík <frostyx@email.cz>
2016-06-15 18:50:42 +0200

    Add copr_full_name property

--- copr_rebuild_tools/copr_backend.py
+++ copr_rebuild_tools/copr_backend.py
@@ -3,6 +3,10 @@ class CoprBackend(object):
     def __init__(self, conf):
         self.conf = conf
 
+    @property
+    def copr_full_name(self):
+        return "/".join(filter(None,[self.conf.get("owner", None), self.conf["project"]]))
+
     def submit_all(self, packages):
         for package in packages:
             self.submit(package)
--- tests/test_copr_backend.py
+++ tests/test_copr_backend.py
@@ -7,7 +7,12 @@ class CoprDummy(CoprBackend):
 
 
 class TestCoprBackend(unittest.TestCase):
-    pass
+    def test_copr_full_name(self):
+        cb1 = CoprDummy({"project": "copr"})
+        self.assertEqual(cb1.copr_full_name, "copr")
+
+        cb2 = CoprDummy({"owner": "@group", "project": "copr"})
+        self.assertEqual(cb2.copr_full_name, "@group/copr")
 
 
 if __name__ == "__main__":
