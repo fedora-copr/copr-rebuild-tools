a1f42040c374e9ff6c72c6e6cd3fa3498a29485b
Jakub Kadlčík <frostyx@email.cz>
2016-06-15 18:01:21 +0200

    Convert conf to dictionary

--- copr_rebuild_tools/config.py
+++ copr_rebuild_tools/config.py
@@ -1,7 +1,11 @@
 import ConfigParser
 
 
-def read(path):
+def read(path, backend):
     conf = ConfigParser.ConfigParser()
     conf.read(path)
-    return conf
+    return to_dict(conf, ["general", backend])
+
+
+def to_dict(conf, sections):
+    return dict([(s, dict(conf.items(s))) for s in sections])
