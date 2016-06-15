bab8d7e87aed2a37ff41b629ddf1c79005a5ee92
Jakub Kadlčík <frostyx@email.cz>
2016-06-15 18:27:08 +0200

    Expand ~ in copr-config value

--- copr_rebuild_tools/config.py
+++ copr_rebuild_tools/config.py
@@ -1,9 +1,11 @@
+import os
 import ConfigParser
 
 
 def read(path, backend):
     conf = ConfigParser.ConfigParser()
     conf.read(path)
+    conf.set("general", "copr-config", os.path.expanduser(conf.get("general", "copr-config")))
     return to_dict(conf, ["general", backend])
 
 
