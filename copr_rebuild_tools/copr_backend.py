99bc6cc2501414f74b2ede47c7efb57684baf993
Jakub Kadlčík <frostyx@email.cz>
2016-06-15 20:05:56 +0200

    Add possibility to sleep between submitting

--- copr_rebuild_tools/copr_backend.py
+++ copr_rebuild_tools/copr_backend.py
@@ -1,3 +1,6 @@
+import time
+
+
 class CoprBackend(object):
 
     def __init__(self, conf):
@@ -7,9 +10,10 @@ class CoprBackend(object):
     def copr_full_name(self):
         return "/".join(filter(None,[self.conf.get("owner", None), self.conf["project"]]))
 
-    def submit_all(self, packages):
+    def submit_all(self, packages, sleep=0):
         for package in packages:
             self.submit(package)
+            time.sleep(sleep)
 
     def submit(self, package):
         """
