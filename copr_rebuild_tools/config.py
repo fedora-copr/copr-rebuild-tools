97ae1db831c272cf1403700b08e06fc3eb32fd5d
Jakub Kadlčík <frostyx@email.cz>
2016-06-15 20:04:11 +0200

    Convert seconds from config to int

--- copr_rebuild_tools/config.py
+++ copr_rebuild_tools/config.py
@@ -5,8 +5,11 @@ import ConfigParser
 def read(path, backend):
     conf = ConfigParser.ConfigParser()
     conf.read(path)
-    conf.set("general", "copr-config", os.path.expanduser(conf.get("general", "copr-config")))
-    return to_dict(conf, ["general", backend])
+
+    confd = to_dict(conf, ["general", backend])
+    confd["copr-config"] = os.path.expanduser(confd["copr-config"])
+    confd["sleep"] = int(confd["sleep"])
+    return confd
 
 
 def to_dict(conf, sections):
