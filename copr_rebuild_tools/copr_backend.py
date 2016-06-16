ddd7dd7fb8c548746c2d4834e6203613aa7be414
Jakub Kadlčík <frostyx@email.cz>
2016-06-16 15:30:07 +0200

    Pass callback function to submit_all

--- copr_rebuild_tools/copr_backend.py
+++ copr_rebuild_tools/copr_backend.py
@@ -10,8 +10,10 @@ class CoprBackend(object):
     def copr_full_name(self):
         return "/".join(filter(None,[self.conf.get("owner", None), self.conf["project"]]))
 
-    def submit_all(self, packages, sleep=0):
+    def submit_all(self, packages, sleep=0, callback=None):
         for package in packages:
+            if callback:
+                callback(package)
             self.submit(package)
             time.sleep(sleep)
 
