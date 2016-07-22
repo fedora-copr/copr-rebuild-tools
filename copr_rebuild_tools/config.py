c7682fae69186365ece71e5e0e832e722499a5e1
Jakub Kadlčík <frostyx@email.cz>
2016-07-22 11:40:44 +0200

    Use default copr config path if it is not set

--- copr_rebuild_tools/config.py
+++ copr_rebuild_tools/config.py
@@ -7,8 +7,9 @@ def read(path, backend):
     conf.read(path)
 
     confd = to_dict(conf, ["general", backend])
-    confd["copr-config"] = os.path.expanduser(confd["copr-config"])
     confd["sleep"] = int(confd["sleep"])
+    confd["copr-config"] = os.path.expanduser(confd["copr-config"]) if "copr-config" in confd else \
+        os.path.join(os.path.expanduser("~"), ".config", "copr")
     return confd
 
 
