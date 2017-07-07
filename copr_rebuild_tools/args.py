ba9a0959a5311dfd6e6ace4d6e4bce75758f80bc
Your Name <you@example.com>
2017-07-07 05:00:57 +0200

    Have common parser parameters for submit and print actions

--- copr_rebuild_tools/args.py
+++ copr_rebuild_tools/args.py
@@ -7,18 +7,23 @@ parser.add_argument("-c", "--config", help="Path to a configuration file")
 parser.add_argument("backend")
 subparsers = parser.add_subparsers(title="actions")
 
-parser_stats = subparsers.add_parser("stats", help="")
-parser_stats.set_defaults(func="action_stats")
 
-parser_submit = subparsers.add_parser("submit", help="")
+parser_base = argparse.ArgumentParser(add_help=False)
+parser_base.add_argument("--previous", help="")
+parser_base.add_argument("--limit", type=int, help="")
+parser_base.add_argument("--new-packages", action="store_true", help="")
+parser_base.add_argument("--new-versions", action="store_true", help="")
+
+
+parser_submit = subparsers.add_parser("submit", parents=[parser_base], help="")
 parser_submit.set_defaults(func="action_submit")
-parser_submit.add_argument("--previous", help="")
-parser_submit.add_argument("--limit", type=int, help="")
-parser_submit.add_argument("--new-packages", action="store_true", help="")
-parser_submit.add_argument("--new-versions", action="store_true", help="")
+
+parser_print = subparsers.add_parser("print", parents=[parser_base], help="")
+parser_print.set_defaults(func="action_print")
+
 
 parser_successful = subparsers.add_parser("successful", help="")
 parser_successful.set_defaults(func="action_successful")
 
-parser_print = subparsers.add_parser("print", help="")
-parser_print.set_defaults(func="action_print")
+parser_stats = subparsers.add_parser("stats", help="")
+parser_stats.set_defaults(func="action_stats")
